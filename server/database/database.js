adfee97631ceee5edf9f6faf568f2733c67d271c
SethZarko <sethzarkovich@gmail.com>
2024-03-11 15:15:18 -0400

    Updating database function

--- server/database/database.js
+++ server/database/database.js
@@ -7,9 +7,14 @@ const mongoURI = process.env.MONGO_URI
 
 export const connectDatabase = async () => {
     try {
+        if (!mongoURI) {
+            throw new Error('MONGO_URI environment variable is missing');
+        }
+
         await mongoose.connect(mongoURI)
         console.log('MongoDB Connected...');
     } catch (error) {
-        console.error('Error')
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
     }
 }
